8fe6e8ee32965b6c449ed2fdb675d92606561d9e
lqu3j <lqu3qf@gmail.com>
2021-08-10 23:15:08 +0800

    combination-sum

--- combination-sum/main.go
+++ combination-sum/main.go
@@ -1,40 +1,38 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
 func main() {
-	log.Println(combinationSum([]int{2, 3, 6, 7}, 7))
+	log.Println(combinationSum([]int{2, 7, 6, 3, 5, 1}, 9))
 }
 
 func combinationSum(candidates []int, target int) [][]int {
+	sort.Sort(sort.IntSlice(candidates))
 	results := [][]int{}
-	if target <= 0 {
-		return results
+	calc(candidates, []int{}, target, &results)
+	return results
+}
+
+func calc(candidates []int, array []int, target int, results *[][]int) {
+
+	if target == 0 {
+		tmp := make([]int, len(array), len(array))
+		copy(tmp, array)
+		*results = append(*results, tmp)
 	}
 
 	for i := 0; i < len(candidates); i++ {
 		sub := target - candidates[i]
+
 		if sub < 0 {
-			return [][]int{}
-		} else if sub == 0 {
-			return [][]int{{candidates[i]}}
+			continue
 		} else {
-			tmp := combinationSum(candidates[i:], target-candidates[i])
+
+			calc(candidates[i:], append(array, candidates[i]), sub, results)
 		}
-
-		// tmp := combinationSum(candidates[i:], target-candidates[i])
-
-		// if len(tmp) == 0 {
-		// 	tmp = append(tmp, []int{candidates[i]})
-		// } else {
-		// 	for j := range tmp {
-		// 		tmp[j] = append(tmp[j], candidates[i])
-		// 	}
-		// }
-
-		// if target-candidates[i] == 0 {
-		// 	results = append(results, tmp...)
-		// }
 	}
-	return results
+
 }
